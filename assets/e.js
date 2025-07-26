1b031f964649c4fc5433174312ba5c5d753a895d
Cqmbo1 <168939007+Cqmbo1@users.noreply.github.com>
2025-07-25 20:48:25 -0400

    UPD

---
+++ assets/e.js
@@ -0,0 +1,13 @@
+  // Create a link element
+  var favicon = document.createElement('link');
+  favicon.rel = 'icon';                // Set rel attribute for favicon
+  favicon.type = 'image/png';           // Set type attribute
+  favicon.href = '/assets/cqmbo__32x32.png'; // Set the URL for the favicon
+
+  // Append the link element to the head
+  document.head.appendChild(favicon);
+
+    if (location.hostname !== "cqmbo1.github.io") {
+      document.body.innerHTML = "Unauthorized mirror detected.";
+      throw new Error("Unauthorized use.");
+    }
